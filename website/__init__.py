977d81087187cf11dd36e193c47369795c8fb9fe
Miles Rack <milesr4@protonmail.com>
2021-05-28 11:14:04 -0400

    Add not found page

--- website/__init__.py
+++ website/__init__.py
@@ -19,7 +19,7 @@ def create_app():
 	app.register_blueprint(pages,url_prefix='/')
 	app.register_blueprint(auth,url_prefix='/')
 
-	from .models import User, Note
+	from .models import User
 
 	create_database(app)
 
