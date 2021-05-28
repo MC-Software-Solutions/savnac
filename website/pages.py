9d5ceed17e032522c5544fdfe1e2ccec4d827158
Miles Rack <milesr4@protonmail.com>
2021-05-28 13:23:36 -0400

    Change course cards

--- website/pages.py
+++ website/pages.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template
 from flask_login import login_required, current_user
 from . import db
 from .models import User
+import requests
 
 pages = Blueprint('pages',__name__)
 
@@ -12,7 +13,11 @@ def home():
 @pages.route('/courses')
 @login_required
 def courses():
-	return render_template('courses.html', user=current_user)
+	url = f'https://{current_user.domain}/api/v1/courses/'
+	params = {'access_token':current_user.api_token.strip(),'enrollment_state':'active','exclude_blueprint_courses':'true','per_page':'100'}
+	r = requests.get(url,params=params)
+	data = [(item['id'], item['name']) for item in r.json()]
+	return render_template('courses.html', user=current_user, data=data)
 
 @pages.route('<stuff>')
 def path(stuff):
